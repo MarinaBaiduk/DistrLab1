1ee06355924bde716a10749d817a832e69c36fde
Marina Baiduk <marinabaiduk@yandex.ru>
2020-05-18 21:08:29 +0700

    init

---
+++ src/main/java/nsu/Baiduk/Main.java
@@ -0,0 +1,13 @@
+package nsu.Baiduk;
+
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
+import javax.xml.stream.events.XMLEvent;
+
+public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args){
+        LOG.info("Hi");
+    }
+}
